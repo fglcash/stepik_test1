0349e36800ef5d824c5f0c8bb643274a5f45bbe8
fglcash <roman2164532@gmail.com>
2025-10-25 20:53:34 +0300

    tt

--- test_items.py
+++ test_items.py
@@ -10,8 +10,7 @@ def test_guest_should_see_add_to_basket_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     
-    # Раскомментируйте следующую строку для визуальной проверки языка
-    # time.sleep(30)
+    time.sleep(30)
     
     # Проверяем наличие кнопки добавления в корзину
     add_to_basket_button = browser.find_elements(By.CSS_SELECTOR,"button.btn-add-to-basket")
