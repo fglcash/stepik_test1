078fd21b8ed16ffdc038761a27c817e77d4e8af8
fglcash <roman2164532@gmail.com>
2025-10-25 20:51:17 +0300

    tt

--- test_items.py
+++ test_items.py
@@ -1,3 +1,11 @@
+import pytest
+import math
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 def test_guest_should_see_add_to_basket_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
@@ -6,5 +14,5 @@ def test_guest_should_see_add_to_basket_button(browser):
     # time.sleep(30)
     
     # Проверяем наличие кнопки добавления в корзину
-    add_to_basket_button = browser.find_elements_by_css_selector("button.btn-add-to-basket")
+    add_to_basket_button = browser.find_elements(By.CSS_SELECTOR,"button.btn-add-to-basket")
     assert len(add_to_basket_button) > 0, "Add to basket button is not found"
